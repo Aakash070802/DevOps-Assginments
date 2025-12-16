40082593145ecfad83b618c4c46bb4856e80c481
Aakash <aakash.kash92@gmail.com>
2025-12-17 00:10:14 +0530

    task 10 is done

--- scripting/Basics-of-Python/file-handling.py
+++ scripting/Basics-of-Python/file-handling.py
@@ -1,11 +1,9 @@
 # f = open("data.txt", "r") read a file
 
-f = open("data.txt", "r+")
-
-data = f.readline()
-
-print(data)
+f = open("data.txt", "w")
 
 f.write("Hello, World!")
+f.write("\nWelcome to file handling in Python.")
+f.write("\nThis is a sample text file.")
 
 f.close()
