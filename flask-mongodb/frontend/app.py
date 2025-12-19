d81c3388baa37fe20926f7556a6e0af4b33f28a1
Aakash <aakash.kash92@gmail.com>
2025-12-20 01:16:41 +0530

    success msg and connection to frontend and backend added

--- flask-mongodb/frontend/app.py
+++ flask-mongodb/frontend/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
+import requests
 
-BACKEND_URL = "http://localhost:5000"
+BACKEND_URL = "http://0.0.0.0:5000"
 
 app = Flask(__name__)
 
@@ -12,6 +13,23 @@ def home():
     current_time = datetime.now().strftime('%H:%M')
     print(day_of_week)
     return render_template('index.html' , day=day_of_week, time=current_time)
+@app.route('/submit', methods=['POST'])
+def submit():
+    form_data = {
+        'username': request.form['username'],
+        'email': request.form['email'],
+        'password': request.form['password'],
+        'cnf-passwrd': request.form['cnf-passwrd']
+    }
+    response = requests.post(f"{BACKEND_URL}/submit", data=form_data)
+
+    result = response.json()
+
+    if result['status'] == 'success':
+        return render_template('success.html', message=result['message'])
+    else:
+        return result['message'], 400
+    
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7000, debug=True)
